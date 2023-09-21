55c8e9fab82cb57c73d061dfde9eb3a3c7a6e6b5
heatingma <115260102+heatingma@users.noreply.github.com>
2023-09-22 01:22:54 +0800

    update font-end

--- sources/font-end/chatting/static/js/light-dark.js
+++ sources/font-end/chatting/static/js/light-dark.js
@@ -1,18 +1,31 @@
-!function(a) {
-    "use strict";
-    a("#light-dark").on("click", function(t) {
-        console.log("click!");
-        if ("disabled" !== a("#bootstrap-style").attr("disabled")) {
-            a("#bootstrap-dark-style").attr("disabled", !1);
-            a("#bootstrap-style").attr("disabled", !0);
-            a("#side-menu-dark-style").attr("disabled", !1);
-            a("#side-menu-style").attr("disabled", !0);
-        } else {
-            a("#bootstrap-dark-style").attr("disabled", !0);
-            a("#bootstrap-style").attr("disabled", !1);
-            a("#side-menu-dark-style").attr("disabled", !0);
-            a("#side-menu-style").attr("disabled", !1);
+var light_dark = document.getElementById('light-dark');
+var flag = 0;
+
+function change_style() {
+    var styles = document.getElementsByTagName('link');
+    if (flag == 0) {
+        // change from light to dark
+        for (var i = 0; i < styles.length; i++) {
+            var style = styles[i];
+            var href = style.getAttribute('href');
+            if (href.startsWith('static/css/light/'))
+                style.disabled = true;
+            if (href.startsWith('static/css/dark/'))
+                style.disabled = false;
         }
-    });
-  
-}(jQuery);
+    } 
+    else {
+        // change from dark to light
+        for (var i = 0; i < styles.length; i++) {
+            var style = styles[i];
+            var href = style.getAttribute('href');
+            if (href.startsWith('static/css/light/'))
+                style.disabled = false;
+            if (href.startsWith('static/css/dark/'))
+                style.disabled = true;
+        }
+    }
+    flag = (flag + 1) % 2;
+}
+
+light_dark.addEventListener('click', change_style);
