6218c08c6db0ab148169bc56a999c97af060721d
heatingma <115260102+heatingma@users.noreply.github.com>
2023-09-27 01:47:29 +0800

    chatroom 0.0.1

--- chat/urls.py
+++ chat/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from chat import views
 
 urlpatterns = [
-    path('', views.visit, name='home'),
+    path('', views.home, name='home'),
     path('my/', views.my, name='my'),
     path('groups/', views.groups, name='groups'),
     path('chatroom/', views.chatroom, name='chatroom'),
--- chat/views.py
+++ chat/views.py
@@ -5,47 +5,7 @@ from .forms import EditProfileForm, RoomForm
 from .models import Profile, Room, RoomMessage
 from users.models import User
 
-@login_required
-def visit(request: HttpRequest):
-    # deal with get method
-    if request.method == "GET":
-        username = request.user.username
-        user = get_object_or_404(User, username=username)
-        profile = get_object_or_404(Profile, user=user)
-        profile_form = EditProfileForm(request.user.username)
-    # deal with post method
-    elif request.method == "POST":
-        profile_form = EditProfileForm(request.user.username, request.POST, request.FILES)
-        # check the profile form
-        if profile_form.is_valid():
-            about_me = profile_form.cleaned_data["about_me"]
-            image = profile_form.cleaned_data["image"]
-            location = profile_form.cleaned_data["location"]
-            user = User.objects.get(username=request.user.username)
-            profile = Profile.objects.get(user=user)
-            if about_me != "":
-                profile.about_me = about_me
-            if image:
-                profile.image = image
-            if location != "":
-                profile.location = location
-            profile.save()
-        else:
-            username = request.user.username
-            user = get_object_or_404(User, username=username)
-            profile = get_object_or_404(Profile, user=user)
-            profile_form = EditProfileForm(request.user.username)
-            
-    return render(
-        request=request,
-        template_name='chat/home.html', 
-        context={
-            'profile': profile,
-            'profile_form': profile_form
-        }
-    )
-    
-    
+
 @login_required
 def chatroom(request: HttpRequest):
     username = request.user.username
@@ -81,18 +41,34 @@ def innerroom(request: HttpRequest, room_name):
     wrong_message = ""
     chat_room, created = Room.objects.get_or_create(name=room_name)
     room_messages = RoomMessage.objects.filter(room=chat_room).order_by('timestamp')
+    cur_room = room_messages[0].room
     return render(
         request=request, 
         template_name='chat/innerroom.html', 
         context={
             'profile': profile,
+            'cur_room': cur_room,
             'rooms': Room.objects.all(),
             'wrong_message': wrong_message,
             'room_messages': room_messages,
         }
     )
     
+
+@login_required
+def home(request: HttpRequest):
+    username = request.user.username
+    user = get_object_or_404(User, username=username)
+    profile = get_object_or_404(Profile, user=user)
+    return render(
+        request=request, 
+        template_name='chat/home.html', 
+        context={
+            'profile': profile,
+        }
+    )
     
+       
 @login_required
 def groups(request: HttpRequest):
     username = request.user.username
@@ -109,16 +85,44 @@ def groups(request: HttpRequest):
     
 @login_required
 def settings(request: HttpRequest):
-    username = request.user.username
-    user = get_object_or_404(User, username=username)
-    profile = get_object_or_404(Profile, user=user)
+    # deal with get method
+    if request.method == "GET":
+        username = request.user.username
+        user = get_object_or_404(User, username=username)
+        profile = get_object_or_404(Profile, user=user)
+        profile_form = EditProfileForm(request.user.username)
+    # deal with post method
+    elif request.method == "POST":
+        profile_form = EditProfileForm(request.user.username, request.POST, request.FILES)
+        # check the profile form
+        if profile_form.is_valid():
+            about_me = profile_form.cleaned_data["about_me"]
+            image = profile_form.cleaned_data["image"]
+            location = profile_form.cleaned_data["location"]
+            user = User.objects.get(username=request.user.username)
+            profile = Profile.objects.get(user=user)
+            if about_me != "":
+                profile.about_me = about_me
+            if image:
+                profile.image = image
+            if location != "":
+                profile.location = location
+            profile.save()
+        else:
+            username = request.user.username
+            user = get_object_or_404(User, username=username)
+            profile = get_object_or_404(Profile, user=user)
+            profile_form = EditProfileForm(request.user.username)
+            
     return render(
-        request=request, 
+        request=request,
         template_name='chat/settings.html', 
         context={
             'profile': profile,
+            'profile_form': profile_form
         }
     )
+    
     
 @login_required
 def my(request: HttpRequest):
