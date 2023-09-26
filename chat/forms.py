849cc08194dea47cf6f269b054a9d6acd94b80b3
heatingma <115260102+heatingma@users.noreply.github.com>
2023-09-26 08:32:55 +0800

    fix fonts problem

--- chat/forms.py
+++ chat/forms.py
@@ -6,26 +6,19 @@ from users.models import User
 ###############################################   
 
 class EditProfileForm(forms.Form):
-    username = forms.CharField(max_length=20)
-    email = forms.CharField(max_length=50)
-    about_me = forms.CharField(widget=forms.Textarea())
-    image_default = forms.BooleanField(initial=True, required=False)
+    about_me = forms.CharField(widget=forms.Textarea(), required=False)
     image = forms.ImageField(required=False)
-
+    location = forms.CharField(max_length=50, required=False)
+    
     def __init__(self, original_username, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.original_username = original_username
-        if 'username' in self.initial:
-            username = self.initial['username']
-            if username:
-                self.fields['image'].initial = username[0].upper()
                 
     def clean_username(self):
         """
         This function throws an exception if the username has already been
         taken by another user
         """
-
         username = self.cleaned_data['username']
         if username != self.original_username:
             # filter!
--- chat/views.py
+++ chat/views.py
@@ -7,40 +7,40 @@ from users.models import User
 
 @login_required
 def visit(request: HttpRequest):
-    username = request.user.username
-    user = get_object_or_404(User, username=username)
-    profile = get_object_or_404(Profile, user=user)
-    # form = EditProfileForm(request.user.username, request.POST, request.FILES)
-    print("profile:", profile.location)
-    
+    # deal with get method
+    if request.method == "GET":
+        username = request.user.username
+        user = get_object_or_404(User, username=username)
+        profile = get_object_or_404(Profile, user=user)
+        profile_form = EditProfileForm(request.user.username)
+    # deal with post method
+    elif request.method == "POST":
+        profile_form = EditProfileForm(request.user.username, request.POST, request.FILES)
+        # check the profile form
+        if profile_form.is_valid():
+            about_me = profile_form.cleaned_data["about_me"]
+            image = profile_form.cleaned_data["image"]
+            location = profile_form.cleaned_data["location"]
+            user = User.objects.get(username=request.user.username)
+            profile = Profile.objects.get(user=user)
+            if about_me != "":
+                profile.about_me = about_me
+            if image:
+                profile.image = image
+            if location != "":
+                profile.location = location
+            profile.save()
+        else:
+            username = request.user.username
+            user = get_object_or_404(User, username=username)
+            profile = get_object_or_404(Profile, user=user)
+            profile_form = EditProfileForm(request.user.username)
+            
     return render(
         request=request,
         template_name='chat/home.html', 
         context={
-            'profile': profile
+            'profile': profile,
+            'profile_form': profile_form
         }
     )
-    
-
-
-@login_required
-def edit_profile(request):
-    if request.method == "POST":
-        form = EditProfileForm(request.user.username,request.POST, request.FILES)
-        if form.is_valid():
-            about_me = form.cleaned_data["about_me"]
-            username = form.cleaned_data["username"]
-            image = form.cleaned_data["image"]
-
-            user = User.objects.get(id=request.user.id)
-            profile = Profile.objects.get(user=user)
-            user.username = username
-            user.save()
-            profile.about_me = about_me
-            if image:
-                profile.image = image
-            profile.save()
-            return redirect("users:profile", username=user.username)
-    else:
-        form = EditProfileForm(request.user.username)
-    return render(request, "users/edit_profile.html", {'form': form})
