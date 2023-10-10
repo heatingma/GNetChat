d4778d00f84e2b0cd5d1b2abdf03c09000ab409d
ccliu-u <2877312905@qq.com>
2023-10-10 19:39:11 +0800

    add-guide-tmp

--- chat/forms.py
+++ chat/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from users.models import User
+from .models import LINK
 
 ###############################################
 #                   Profile                   #
@@ -78,3 +79,10 @@ class ConfirmDeleteChatroomForm(forms.Form):
 class SendInvitationForm(forms.Form):
     invite_email = forms.CharField(required=True)
     invite_message = forms.CharField(widget=forms.Textarea(), required=False) 
+
+class linkform(forms.Form):
+    add_name = forms.CharField(required=True)
+    add_link = forms.CharField(required=True)
+
+class Deletelinkform(forms.Form):
+    delete_name = forms.CharField(required=True)
