b078815645f4d6423d535be6d0a98808a174bffe
heatingma <115260102+heatingma@users.noreply.github.com>
2024-02-28 19:08:07 +0800

    public

--- users/check_invatation.py
+++ users/check_invatation.py
@@ -1,11 +1,12 @@
 import numpy as np
 
-
 def check_invatation_code(code:str):
-    hash_codes = np.load("users/hash.npy", allow_pickle=True)
-    return True if code in hash_codes else False
-
-    
+    # hash_codes = np.load("users/hash.npy", allow_pickle=True)
+    # return True if code in hash_codes else False
+    if code == "gnetchat":
+        return True
+    else:
+        return False
 
 def delete_invatation_code(code:str):
     hash_codes = np.load("users/hash.npy", allow_pickle=True)
